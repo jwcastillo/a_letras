d4f875dca9faff4df8b9438e8e41c713e4a3f692
jwcastillo <jwcastillo@gmail.com>
2016-01-03 21:49:56 -0300

    funciona unidades

--- lib/a_letras.rb
+++ lib/a_letras.rb
@@ -1,11 +1,13 @@
 module ALetras
   @unidades = { 1 => 'uno', 2 => 'dos', 3 => 'tres', 4 => 'cuatro', 5 => 'cinco', 6 => 'seis', 7 => 'siete', 8 => 'ocho', 9 => 'nueve' }
 
-  def unidad(num)
-    @unidades[num]
+  class << self
+      def unidad(num)
+        @unidades[num]
+      end
   end
 
-  def numero(num)
+  def self.numero(num)
     num = num.to_i
     unidad(num)
     end

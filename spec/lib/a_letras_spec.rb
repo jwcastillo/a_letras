92d52dbf6b255fcd82d3ee973dc5bcd5518d0b2e
jwcastillo <jwcastillo@gmail.com>
2016-01-03 21:45:33 -0300

    test inicial

--- spec/lib/a_letras_spec.rb
+++ spec/lib/a_letras_spec.rb
@@ -1,4 +1,8 @@
 require 'a_letras'
 
 RSpec.describe ALetras do
+  context 'cuando el numero es mayor de 0' do
+    it { expect(ALetras.numero(1)).to eq('uno') }
+    it { expect(ALetras.numero(5)).to eq('cinco') }
+  end
 end

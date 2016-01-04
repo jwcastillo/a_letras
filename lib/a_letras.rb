575893faa9d01a50530bc229f83b2b5647f340de
jwcastillo <jwcastillo@gmail.com>
2016-01-03 21:48:12 -0300

    unidades

--- lib/a_letras.rb
+++ lib/a_letras.rb
@@ -1,4 +1,12 @@
 module ALetras
-  def self.numero(_num)
+  @unidades = { 1 => 'uno', 2 => 'dos', 3 => 'tres', 4 => 'cuatro', 5 => 'cinco', 6 => 'seis', 7 => 'siete', 8 => 'ocho', 9 => 'nueve' }
+
+  def unidad(num)
+    @unidades[num]
+  end
+
+  def numero(num)
+    num = num.to_i
+    unidad(num)
     end
 end
